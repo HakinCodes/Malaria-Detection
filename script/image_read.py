bc79aef00de76f4a5453c458d0d6cb6bd5f2d046
Restyled.io <commits@restyled.io>
2020-08-21 13:18:31 +0000

    Restyled by black

--- script/image_read.py
+++ script/image_read.py
@@ -13,8 +13,8 @@ UNINFECTED = os.listdir("../content/cell_images/cell_images/Uninfected/")
 
 for i in PARASITIZED:
     try:
-        image = cv2.imread("../content/cell_images/cell_images/Parasitized/"+i)
-        image_from_array = Image.fromarray(image, 'RGB')
+        image = cv2.imread("../content/cell_images/cell_images/Parasitized/" + i)
+        image_from_array = Image.fromarray(image, "RGB")
         size_image = image_from_array.resize((50, 50))
         DATA.append(np.array(size_image))
         LABELS.append(0)
@@ -23,8 +23,8 @@ for i in PARASITIZED:
 
 for u in UNINFECTED:
     try:
-        image = cv2.imread("../content/cell_images/cell_images/Uninfected/"+u)
-        image_from_array = Image.fromarray(image, 'RGB')
+        image = cv2.imread("../content/cell_images/cell_images/Uninfected/" + u)
+        image_from_array = Image.fromarray(image, "RGB")
         size_image = image_from_array.resize((50, 50))
         DATA.append(np.array(size_image))
         LABELS.append(1)
@@ -36,8 +36,8 @@ CELLS = np.array(DATA)
 LABELS = np.array(LABELS)
 
 # Saving data as a numpy array.
-np.save('Cells', CELLS)
-np.save('Labels', LABELS)
+np.save("Cells", CELLS)
+np.save("Labels", LABELS)
 
 # for loading saved data
 CELLS = np.load("Cells.npy")
