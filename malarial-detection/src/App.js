306c67d5a606d962055df68c740e352a6feb9ca1
Restyled.io <commits@restyled.io>
2020-08-21 09:54:37 +0000

    Restyled by clang-format

--- malarial-detection/src/App.js
+++ malarial-detection/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -11,15 +13,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  className = "App-link"
+  href = "https://reactjs.org"
+  target = "_blank"
+  rel = "noopener noreferrer" > Learn React</a>
+      </header><
+        /div>
   );
 }
 
