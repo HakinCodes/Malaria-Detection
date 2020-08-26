dc029d4f2e8e91476abb6105aa441059c4c23b47
Restyled.io <commits@restyled.io>
2020-08-26 19:07:06 +0000

    Restyled by prettier

--- malarial-detection/src/App.js
+++ malarial-detection/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import React from "react";
-import Router from "./components/router/router"
+import Router from "./components/router/router";
 
 function App() {
-  return (<div className = "App"><header className = "App-header"><Router />
-          </header>
-    </div>);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Router />
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- malarial-detection/src/components/router/router.js
+++ malarial-detection/src/components/router/router.js
@@ -1,24 +1,20 @@
 import React from "react";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const Router = () =>
-    (<BrowserRouter><Switch>
-     <Route path = "/about" component =
-      { About } />
-        <Route path="/demo " component={Demo}/>
-      < Route path = "/" component = { Home } />
-      </Switch>
-     </BrowserRouter>
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/about" component={About} />
+      <Route path="/demo " component={Demo} />
+      <Route path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>
 );
 
-const Home = () => (
-  <h2>Home Page</h2>);
+const Home = () => <h2>Home Page</h2>;
 
-const About = () =>
-    (<h2>About Page</h2>
-);
+const About = () => <h2>About Page</h2>;
 
-const Demo = () => (
-  <h2>Demo Page</h2>);
+const Demo = () => <h2>Demo Page</h2>;
 
 export default Router;
