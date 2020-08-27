1b35e110d8a4d04351f31e6f6b3091cea6d4d68a
Restyled.io <commits@restyled.io>
2020-08-27 05:23:41 +0000

    Restyled by clang-format

--- malarial-detection/src/App.js
+++ malarial-detection/src/App.js
@@ -16,16 +16,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          Learn React
-        </a>
-      </header>
-    </div>
+  className = "App-link"
+  href = "https://reactjs.org"
+  target = "_blank"
+  rel = "noopener noreferrer" > {" "} Learn React</a>
+      </header><
+        /div>
   );
 }
 
--- malarial-detection/src/components/Navbar/Navbar.js
+++ malarial-detection/src/components/Navbar/Navbar.js
@@ -1,42 +1,38 @@
-import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import React, {useState} from 'react';
 import {
   Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
   Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
   NavItem,
   NavLink,
 } from 'reactstrap';
 
-import 'bootstrap/dist/css/bootstrap.css';
+const HeaderNavbar =
+    () => {
+      const [isOpen, setIsOpen] = useState(false);
 
-const HeaderNavbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+      const toggle = () => setIsOpen(!isOpen);
 
-  const toggle = () => setIsOpen(!isOpen);
-
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Malaria Detection</NavbarBrand>
+      return (
+          <div><Navbar color = "light" light expand = "md">
+          <NavbarBrand href = "/">Malaria
+              Detection</NavbarBrand>
         <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="/">Home</NavLink>
+          <Collapse isOpen = {isOpen} navbar><Nav className = "ml-auto" navbar>
+          <NavItem><NavLink href = "/">Home</NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink href="/about">About</NavLink>
+          <NavItem>
+          <NavLink href = "/about">About</NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink href="/demo">Demo</NavLink>
+          <NavItem><NavLink href = "/demo">Demo</NavLink>
             </NavItem>
           </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
-}
+        </Collapse></Navbar>
+    </div>);
+    }
 
 export default HeaderNavbar;
