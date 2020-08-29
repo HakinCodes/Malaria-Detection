d83d67678e2f844e9358dc024de50bd72797e3f5
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-08-29 15:10:30 +0500

    done with styling

---
+++ malarial-detection/src/Pages/App.js
@@ -0,0 +1,15 @@
+import "Styles/App.css";
+import React from "react";
+import Router from "Components/Router/router";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Router />
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- malarial-detection/src/Pages/index.js
+++ malarial-detection/src/Pages/index.js
@@ -1,9 +1,9 @@
-import "./index.css";
+import "Styles/index.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import App from "Pages/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
