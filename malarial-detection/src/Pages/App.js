3b9acf2715307b33149e46c9e61517fc190f923e
Restyled.io <commits@restyled.io>
2020-08-29 10:29:48 +0000

    Restyled by prettier

--- malarial-detection/src/Pages/App.js
+++ malarial-detection/src/Pages/App.js
@@ -4,9 +4,13 @@ import Router from "Components/Router/router";
 import React from "react";
 
 function App() {
-  return (<div className = "App"><header className = "App-header"><Router />
-          </header>
-    </div>);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Router />
+      </header>
+    </div>
+  );
 }
 
 export default App;
