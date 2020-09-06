8ea231519befb8ce6e4b61f8f576b4ae53a5c1eb
Restyled.io <commits@restyled.io>
2020-09-06 15:11:39 +0000

    Restyled by clang-format

--- malarial-detection/src/App.js
+++ malarial-detection/src/App.js
@@ -5,14 +5,9 @@ import HeaderNavbar from "./components/Navbar/Navbar";
 import Router from "./components/router/router"
 
 function App() {
-  return (
-    <div className="App">
-      <HeaderNavbar />
-      <header className="App-header">
-        <Router/>
-      </header>
-    </div>
-  );
+  return (<div className = "App"><HeaderNavbar />
+          <header className = "App-header"><Router /></header>
+    </div>);
 }
 
 export default App;
--- malarial-detection/src/components/router/router.js
+++ malarial-detection/src/components/router/router.js
@@ -1,24 +1,20 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Switch,
-  Route
-} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
 import Homepage from '../pages/Homepage';
 
-const Router = () => (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/about" component={About}/>
-        <Route path="/demo" component={Demo}/>
-        <Route path="/" component={Homepage}/>
+const Router = () =>
+    (<BrowserRouter><Switch>
+     <Route path = "/about" component =
+      { About } />
+        <Route path="/demo " component={Demo}/>
+      < Route path = "/" component = { Homepage } />
       </Switch>
-    </BrowserRouter>
+     </BrowserRouter>
 );
 
 const About = () => (
-  <h2>About Page</h2>
-);
+  <h2>About Page</h2>);
 
 const Demo = () => (
   <h2>Demo Page</h2>
