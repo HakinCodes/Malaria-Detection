b9d274b252c98bbc3b035ddbce0c75a540404cc0
sameersrivastava13 <sameersrivastava.193@gmail.com>
2020-09-18 14:53:04 +0530

    Solved #146 Added : Unit-Tests for the Flask Application

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+def test_index(app, client):
+    res = client.get('/')
+    assert res.status_code == 200
+
+def test_form(app, client):
+    res = client.get('/form')
+    assert res.status_code == 200
+
+def test_result(app, client):
+    res = client.get('/result')
+    assert res.status_code == 200
