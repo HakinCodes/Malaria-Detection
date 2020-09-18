37eccb13d931520b3a11f6a2237c2738f9e23547
Restyled.io <commits@restyled.io>
2020-09-18 15:49:54 +0000

    Restyled by black

--- models/app.py
+++ models/app.py
@@ -6,24 +6,28 @@ from tkinter import filedialog
 from tkinter import messagebox
 
 
-model = load_model('CNN.h5')
+model = load_model("CNN.h5")
 
-classes = {0: 'Parasitized', 1: 'Uninfected'}
+classes = {0: "Parasitized", 1: "Uninfected"}
 
 
-def predict(path):  # Method for predicting whether the image is of parasitized cell or an uninfected cell
+def predict(
+    path,
+):  # Method for predicting whether the image is of parasitized cell or an uninfected cell
     img = cv2.imread(path)
     img = cv2.resize(img, (50, 50))
     img = img.reshape(-1, 50, 50, 3)
-    img = img/255.0
+    img = img / 255.0
     prd = model.predict(img)
-    certainty = (np.amax(prd)*100)
+    certainty = np.amax(prd) * 100
     index = prd.argmax()  # Selecting Best Estimate
-    messagebox.showinfo("Result", "This cell is {} with {}% certainty.".format(
-        classes[index], certainty))
+    messagebox.showinfo(
+        "Result",
+        "This cell is {} with {}% certainty.".format(classes[index], certainty),
+    )
 
 
-print('Please Enter The Image Of The Cell To Be Diagnosed: ', end='')
+print("Please Enter The Image Of The Cell To Be Diagnosed: ", end="")
 
 root = tk.Tk()
 root.withdraw()
