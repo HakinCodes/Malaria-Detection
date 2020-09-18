82b62a4ff092fe98c3307294276d0189157248b3
Restyled.io <commits@restyled.io>
2020-09-18 15:49:55 +0000

    Restyled by isort

--- models/app.py
+++ models/app.py
@@ -1,10 +1,9 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
 import cv2
 import numpy as np
 from keras.models import load_model
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import messagebox
-
 
 model = load_model("CNN.h5")
 
