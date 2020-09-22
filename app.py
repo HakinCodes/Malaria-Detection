8afc61dfd8494a2e719db4607a719aacbba67110
Restyled.io <commits@restyled.io>
2020-09-22 19:50:41 +0000

    Restyled by autopep8

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for,request, redirect
+from flask import Flask, render_template, url_for, request, redirect
 import numpy as np
 import tensorflow as tf
 from keras.models import load_model
@@ -12,20 +12,23 @@ app = Flask(__name__)
 model = load_model('models/CNN.h5')
 graph = tf.get_default_graph()
 
-def ValuePredictor(np_arr):   
+
+def ValuePredictor(np_arr):
     global graph
     with graph.as_default():
         result = model.predict(np_arr)
     return result[0]
 
+
 def image_preprocess(img):
-  new_shape = (50, 50, 3)
-  img = image.load_img(img)
-  image_array = image.img_to_array(img)
-  image_array = transform.resize(image_array, new_shape, anti_aliasing = True)
-  image_array /= 255
-  image_array = np.expand_dims(image_array, axis = 0)
-  return image_array
+    new_shape = (50, 50, 3)
+    img = image.load_img(img)
+    image_array = image.img_to_array(img)
+    image_array = transform.resize(image_array, new_shape, anti_aliasing=True)
+    image_array /= 255
+    image_array = np.expand_dims(image_array, axis=0)
+    return image_array
+
 
 @app.route("/")
 def landingPage():
@@ -37,7 +40,7 @@ def inputForm():
     return render_template('form.html')
 
 
-@app.route("/result", methods = ['GET', 'POST'])
+@app.route("/result", methods=['GET', 'POST'])
 def result():
     if request.method == 'POST':
         image = request.form['image']
@@ -49,7 +52,7 @@ def result():
             pred = "This cell is likely to be infected with Malarial Parsite"
         print(pred)
         return render_template('result.html', image=image)
-    
+
 
 @app.errorhandler(404)
 # inbuilt function which takes error as parameter
