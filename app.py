a57b3b4da01a18ba179f9e8186fb4450ee1d5895
sameersrivastava13 <sameersrivastava.193@gmail.com>
2020-09-23 16:57:59 +0530

    minor tweaks

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask import jsonify
 from flask import render_template
 import json
 
-with open("config.json", "r") as c:
-    params = json.load(c)["params"]
-
 
 app = Flask(__name__)
 
+
+with open("config.json", "r") as c:
+    params = json.load(c)["params"]
+    
 
 @app.route("/")
 def landingPage():
