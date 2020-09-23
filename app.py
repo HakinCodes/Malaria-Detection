d03982c7741d8e19a8aadd18e9d59147ceda2df7
sameersrivastava13 <sameersrivastava.193@gmail.com>
2020-09-23 12:53:12 +0530

    Solved #168 added : Contributor's Page

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask import jsonify
 from flask import render_template
+import json
+
+with open("config.json",'r') as c:
+    params=json.load(c)['params']
+
 
 app = Flask(__name__)
 
@@ -19,6 +24,9 @@ def inputForm():
 def result():
     return render_template("result.html")
 
+@app.route("/team")
+def team():
+    return render_template("team.html",params=params)
 
 @app.errorhandler(404)
 # inbuilt function which takes error as parameter
